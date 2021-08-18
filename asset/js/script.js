56d6b08a95bc6ee7d022b933625ee63cab4ebf17
Bastocurrency1999 <bastohenna@gmail.com>
2021-08-17 21:39:03 -0700

    make api calls using jquery and create validation for the input

--- asset/js/script.js
+++ asset/js/script.js
@@ -11,8 +11,40 @@ gBtn.onclick = function showNextContainer()
     inputContainer.style.display = 'block';
 }
 
+var checkWeatherBtn = document.querySelector('.c-w-btn');
+var goBackBtn = document.querySelector('.bk-btn');
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Abeokuta&appid=fb76d87eef777c3d39c1628ad382c9b2", function(data){
-    console.log(data);
+checkWeatherBtn.onclick = function displayWeatherPage()
+{
+    var cityName = document.querySelector('.cityName').value;
+    if(cityName == undefined || cityName == ""){
+        alert("Make sure you input your city name");
+    }else{
+    inputContainer.style.display = 'none';
+    descriptionContainer.style.display = 'flex';
+
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=fb76d87eef777c3d39c1628ad382c9b2", function(data){
+        console.log(data);
+        var Icon ="https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    
+        var Description = data.weather[0].description;
+        $('.weather').append(Description)
+    
+        var Name = data.name;
+        $('.name').append(Name)
+    
+        var Temperature = data.main.temp;
+        $('.temp').append(Temperature)
+    
+        // append the icon from the weather to the img with class of w-icon
+        $('.w-icon').attr("src", Icon);
+    }
+    );
 }
-);
+}
+goBackBtn.onclick = function goBack()
+{
+    descriptionContainer.style.display = 'none';
+    landingContainer.style.display = 'block';
+    document.querySelector('.cityName').value = "";
+}
